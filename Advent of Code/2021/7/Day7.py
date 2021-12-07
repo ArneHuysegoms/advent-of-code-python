4fac7a3e78fa74e31d8638304e3f01f9c80a95e3
Arne <arhuy@hotmail.com>
2021-12-07 15:55:28 +0100

    Day 7 cleanup

--- Advent of Code/2021/7/Day7.py
+++ Advent of Code/2021/7/Day7.py
@@ -1,27 +1,17 @@
-def median(lst):
-    lst.sort()  # Sort the list first
-    if len(lst) % 2 == 0:  # Checking if the length is even
-        # Applying formula which is sum of middle two divided by 2
-        return (lst[len(lst) // 2] + lst[(len(lst) - 1) // 2]) / 2
-    else:
-        # If length is odd then get middle value
-        return lst[len(lst) // 2]
-
-def getFuelCost(list, median):
+def getFuelCost(crabs, median):
     count = 0
-    for i in list:
+    for i in crabs:
         count += abs(i - median)
     return count
+
 
 def findSolution(list):
     input = list[0]
     inputList = [int(s) for s in input.strip().split(',')]
-    print(inputList)
     #16,1,2,0,4,2,7,1,2,14
     #0,1,1,2,2,2,4,7,14,16
     inputList.sort()
     median = inputList[len(inputList) // 2]
-
     return getFuelCost(inputList, median)
 
 
